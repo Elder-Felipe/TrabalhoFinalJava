526fa096b33a876be116a0287dabcee7a867e46f
Elder Felipe <159585921+Elder-Felipe@users.noreply.github.com>
2024-09-30 22:07:20 -0300

    Add files via upload
    Finalizei a entrada e o processamento de dados de todo o código. Falta apenas comentar para ficar perfeito.

--- Trabalho_Final/src/trabalho_final/Metodos_Calculadora.java
+++ Trabalho_Final/src/trabalho_final/Metodos_Calculadora.java
@@ -10,28 +10,28 @@ import javax.swing.JOptionPane;
  *
  * @author 18320283647
  */
-public class Metodos_Calculadora {
+public class Metodos_Calculadora { 
 
 
-public String soma (String num1,String num2){
+public String soma (String num1,String num2){ //INICIO DO METODO SOMA
     double num1Double = Double.parseDouble(num1);
     double num2Double = Double.parseDouble(num2);
     double resultado = num1Double + num2Double;
     
     return String.valueOf(resultado); //converte o Double em String de maneira mais facil
 
-}
+}//FIM DO METODO SOMA
    
-public String sub (String num1,String num2){
+public String sub (String num1,String num2){//INICIO DO METODO SUBTRAÇÃO
     double num1Double = Double.parseDouble(num1);
     double num2Double = Double.parseDouble(num2);
     double resultado = num1Double - num2Double;
     
     return String.valueOf(resultado); //converte o Double em String de maneira mais facil
 
-}
+}//FIM DO METODO SUBTRAÇÃO
 
-public String div (String num1,String num2){
+public String div (String num1,String num2){//INICIO DO METODO DIVISÃO
     if (num1.equals("0")||num2.equals("0")){
        JOptionPane.showMessageDialog(null,"ERRO!!!NENHUM NUMERO È DIVISIVEL POR ZERO");
        return "";
@@ -41,10 +41,17 @@ public String div (String num1,String num2){
     double resultado = num1Double / num2Double;
     return String.valueOf(resultado); //converte o Double em String de maneira mais facil
     }
-}
+}//FIM DO METODO DIVISÃO
 
 
+public String mult (String num1,String num2){ //INICIO DO METODO MULTIPLICAÇÃO
+    double num1Double = Double.parseDouble(num1);
+    double num2Double = Double.parseDouble(num2);
+    double resultado = num1Double * num2Double;
+    
+    return String.valueOf(resultado); //converte o Double em String de maneira mais facil
 
+}//FIM DO METODO MULTIPLICAÇÂO
 
 
 
--- Trabalho_Final/src/trabalho_final/Trabalho_Final.java
+++ Trabalho_Final/src/trabalho_final/Trabalho_Final.java
@@ -20,19 +20,19 @@ public class Trabalho_Final {
         int repetirInt, i = 0; // DECLARAÇÃO DE VARIAVEIS ESSENCIAIS PARA TODOS OS LOOPS
         JOptionPane.showMessageDialog(null, "O OBJETIVO DESTE CÓDIGO É MOSTRAR QUE NÃO ADIANTA TER TALENTO SE NÃO HOUVER AMBIÇÃO"); // PEQUENA MENSAGEM MOTIVACIONAL SEM IMPORTANCIA NO FUNCIONAMENTO DO CODIGO
         do { //LOOP DE REPETIÇÃO DE TODOS OS SWITCH, DA UMA SEGUNDA CHANCE PARA OS USUARIOS
-            qual = JOptionPane.showInputDialog(null, "QUAL PROGRAMA DESEJA USA? \n \n 1.LAÇOS DE REPETIÇÃO \n 2.CALCULADORA ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE); //ENTRADA DE DADOS FUNDAMENTAL PARA A ESCOLHA DO CASE(DO PRIMEIRO SWITCH) 
+            qual = JOptionPane.showInputDialog(null, "QUAL PROGRAMA DESEJA USA? \n \n 1.LAÇOS DE REPETIÇÃO \n 2.CALCULADORA \n 3.CHURRASCO ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE); //ENTRADA DE DADOS FUNDAMENTAL PARA A ESCOLHA DO CASE(DO PRIMEIRO SWITCH) 
 
             switch (qual) { //PRIMEIRO SWITCH, TEM A FUNÇÃO DE DECIDIR QUAL DAS QUESTOES SERÁ EXECULTADA 
 
-                case "1": //PRIMEIRO CASE 1, ENTRA NA QUESTAO N° 13, LAÇOS DE REPETIÇAO
+                case "1": //PRIMEIRO CASE 1, ENTRA NA QUESTAO N° 11, LAÇOS DE REPETIÇAO
 
                     qualcase = JOptionPane.showInputDialog(null, "QUAL LOOP DESEJA USA? \n \n 1.FOR \n 2.DO WHILE \n 3.WHILE ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE); //ENTRADA DE DADOS RESPONSAVEL PARA SABER EM QUAL CASE O PROGRAMA IRA ENTRAR
                     switch (qualcase) {// SEGUNDO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE OS LOOP'S FOR, DO WHILE E WHILE DA QUESTAO 13
 
-                        case "1":// SEGUNDO CASE 1,RESPONSAVEL PELA ESCOLHA DO LOOP FOR DA QUESTAO 13
+                        case "1":// SEGUNDO CASE 1,RESPONSAVEL PELA ESCOLHA DO LOOP FOR DA QUESTAO 11
                             qualabc = JOptionPane.showInputDialog(null, "QUAL ALTERNATIVA PARA O LOOP FOR? \n \n 1.100 A 0 \n 2.0 A 101 \n 3.RANGE ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE);
-                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 13
-                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 13
+                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 11
+                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 11
                                     System.out.println("");
                                     System.out.println("FOR: 100 A 0");
                                     System.out.println("");
@@ -89,8 +89,8 @@ public class Trabalho_Final {
 //----------------------------------------------------------------------------------------------------------------------------------------------------------/            
                         case "2":
                             qualabc = JOptionPane.showInputDialog(null, "QUAL ALTERNATIVA PARA O LOOP DO WHILE? \n \n 1.100 A 0 \n 2.0 A 101 \n 3.RANGE ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE);
-                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 13
-                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 13
+                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 11
+                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 11
                                     i = 100;
                                     System.out.println("");
                                     System.out.println("100 A 0 DO WHILE:");
@@ -153,9 +153,9 @@ public class Trabalho_Final {
                         case "3": // CASE REFERENTE AO WHILE
                             qualabc = JOptionPane.showInputDialog(null, "QUAL ALTERNATIVA PARA O LOOP WHILE? \n \n 1.100 A 0 \n 2.0 A 101 \n 3.RANGE ", "BEM VINDO", JOptionPane.INFORMATION_MESSAGE);
 
-                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 13
+                            switch (qualabc) { //TERCEIRO SWITCH, RESPONSAVEL PELA ESCOLHA ENTRE A, B E C DA QUESTAO 11
 
-                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 13 
+                                case "1": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 11 
                                     i = 100;
                                     System.out.println("");
                                     System.out.println("100 A 0 WHILE");
@@ -168,7 +168,7 @@ public class Trabalho_Final {
                                     }
                                     break; //break do case 1
 
-                                case "2": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 13 
+                                case "2": //TERCEIRO CASE 1, RESPONSAVEL PELA ESCOLHA A DA QUESTAO 11
                                     i = 0;
                                     System.out.println("");
                                     System.out.println("0 A 101 WHILE");
@@ -219,7 +219,7 @@ public class Trabalho_Final {
                     }//FIM DO SEGUNDO SWITCH
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------- /            
-                    //FIM DO LOOP WHILE ----------------------------- INICIO DO  ----------------------------------------------------------------------------------- /
+                    //FIM DO LOOP WHILE ----------------------------- INICIO DO CASE 2 DO PRIMEIRO SWITCH  ----------------------------------------------------------------------------------- /
 //----------------------------------------------------------------------------------------------------------------------------------------------------------/ 
                     break;//break do segundo switch
 
@@ -227,7 +227,7 @@ public class Trabalho_Final {
                     String qualoperador = JOptionPane.showInputDialog("QUAL OPERADOR DESEJA USAR? \n\n 1.'+' \n 2.'-' \n 3. '/' \n 4.'*' ");
                     switch (qualoperador) {
                         case "1":
-                            Metodos_Calculadora soma = new Metodos_Calculadora(); //INICIANDO O METODO DA SOMA-
+                            Metodos_Calculadora soma = new Metodos_Calculadora(); //INICIANDO O METODO DA SOMA
                             String num1 = JOptionPane.showInputDialog("DIGITE O PRIMEIRO NUMERO DA SOMA");
                             String num2 = JOptionPane.showInputDialog("DIGITE O SEGUNDO NUMERO DA SOMA");
                             String resultado = soma.soma(num1, num2); //ENVIANDO OS VALORES PARA O METODO E RECEBENDO O RESULTADO
@@ -237,7 +237,7 @@ public class Trabalho_Final {
 
                         case "2":
                             
-                            Metodos_Calculadora sub = new Metodos_Calculadora(); //INICIANDO O METODO DA SOMA-
+                            Metodos_Calculadora sub = new Metodos_Calculadora(); //INICIANDO O METODO DA SUBTRAÇÃO
                             num1 = JOptionPane.showInputDialog("DIGITE O PRIMEIRO NUMERO DA SUBTRAÇÃO");
                             num2 = JOptionPane.showInputDialog("DIGITE O SEGUNDO NUMERO DA SUBTRAÇÃO");
                             resultado = sub.sub(num1, num2); //ENVIANDO OS VALORES PARA O METODO E RECEBENDO O RESULTADO
@@ -248,18 +248,69 @@ public class Trabalho_Final {
                             
                         case "3":
                             
-                            Metodos_Calculadora div = new Metodos_Calculadora(); //INICIANDO O METODO DA SOMA-
+                            Metodos_Calculadora div = new Metodos_Calculadora(); //INICIANDO O METODO DA DIVISÃO
                             num1 = JOptionPane.showInputDialog("DIGITE O DIVIDENDO DA DIVISÃO");
                             num2 = JOptionPane.showInputDialog("DIGITE O DIVISOR DA DIVISÃO");
                             resultado = div.div(num1, num2); //ENVIANDO OS VALORES PARA O METODO E RECEBENDO O RESULTADO
                             resultDouble = Double.parseDouble(resultado); //CONVERTENDO O VALOR DE STRING PARA DOUBLE
                             JOptionPane.showMessageDialog(null, "O RESULTADO DA DIVISÃO É " + resultDouble);
-                            break;//break do case 2 do switch qualoperador    
+                            break;//break do case 3 do switch qualoperador    
+                        
+                         case "4":
+                            
+                            Metodos_Calculadora mult = new Metodos_Calculadora(); //INICIANDO O METODO DA MULTIPLICAÇÃO
+                            num1 = JOptionPane.showInputDialog("DIGITE O DIVIDENDO DA MULTIPLICAÇÃO");
+                            num2 = JOptionPane.showInputDialog("DIGITE O DIVISOR DA MULTIPLICAÇÃO");
+                            resultado = mult.mult(num1, num2); //ENVIANDO OS VALORES PARA O METODO E RECEBENDO O RESULTADO
+                            resultDouble = Double.parseDouble(resultado); //CONVERTENDO O VALOR DE STRING PARA DOUBLE
+                            JOptionPane.showMessageDialog(null, "O RESULTADO DA MULTIPLICAÇÃO É " + resultDouble);
+                            break;//break do case 3 do switch qualoperador    
+                        
+                        
+                        
+                        
+                        
                         default: //default do Switch qualoperrador
                             JOptionPane.showMessageDialog(null, "ERROR!!! DIGITE UM VALOR VALIDO");
                             break; // break default
                     }//fim switch qualoperador
                     break;//break do case 2 do primeiro switch
+                    
+                case "3":    
+                    String quantpessoas,refri,cerveja,suco;
+                    double carneboi,carneporco,asinha,linguica=0.0;
+                    double quantsuco,quantrefri,quantcerveja=0.0;
+                    double totalboi,totalporco,totalasinha,totalliguica=0.0;
+                    quantpessoas= JOptionPane.showInputDialog("PARA CALCULAR OS GASTOS, PRECISO SABER QUANTAS PESSOAS IRÃO PARTICIPAR NO CHURRASCO");
+                    refri= JOptionPane.showInputDialog("DAS "+quantpessoas+" PESSOAS QUANTAS VÃO BEBER REFRIGERANTE?");
+                    suco= JOptionPane.showInputDialog("DAS "+quantpessoas+" PESSOAS QUANTAS VÃO BEBER SUCO?");
+                    cerveja= JOptionPane.showInputDialog("DAS "+quantpessoas+" PESSOAS QUANTAS VÃO BEBER CERVEJA?");
+                    double refriDouble= Double.parseDouble(refri);
+                    double cervejaDouble=Double.parseDouble(cerveja);
+                    double sucoDouble=Double.parseDouble(suco);
+                    double quantpessoasDouble=Double.parseDouble(quantpessoas);
+                    quantsuco = (sucoDouble*250)/1000;
+                    quantrefri = (refriDouble*500)/1000;
+                    quantcerveja = (cervejaDouble*3000)/1000;
+                    carneboi=(150*quantpessoasDouble)/1000;
+                    carneporco=(80*quantpessoasDouble)/1000;
+                    asinha=(100*quantpessoasDouble)/1000;
+                    linguica=(80*quantpessoasDouble)/1000;
+                    totalboi=(carneboi*40.00);
+                    totalporco=(carneporco*30.00);
+                    totalasinha=(asinha*20.00);
+                    totalliguica=(linguica*15.00);
+                    double total=totalasinha+totalboi+totalporco+totalliguica;
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    JOptionPane.showMessageDialog(null,quantpessoasDouble+" PESSOAS IRÃO PARTICIPAR DO CHURRASCO. \n\n"+cerveja+" PESSOAS IRÃO BEBER CERVEJA. \n"+suco+" PESSOAS IRÃO BEBER SUCO. \n"+refri+" PESSOAS IRÃO BEBER REFRIGERANTES.\n\n QUANTIDADE DE BEBIDAS:\n\n CERVEJA: "+quantcerveja+" L\n\n SUCO: "+quantsuco+" L\n\n REFRIGERANTES: "+quantrefri+" L \n\n QUANTIDADE DE COMIDA E PREÇO \n\n CARNE DE BOI: R$"+totalboi+" \n CARNE DE PORCO: R$"+totalporco+" \n ASINHA: R$"+totalasinha+"\n LINGUIÇA: R$"+totalliguica+" \n\n VALOR TOTAL EM COMIDA: \n R$"+total
+                    );
+                    break; //break do case 3 do primeiro switch
                 default://PRIMEIRO SWITCH
                     JOptionPane.showMessageDialog(null, "DIGITE UM VALOR VALIDO!!!");
                     break;
